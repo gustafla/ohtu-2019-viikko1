c4eff9e00191cb61ff47c21dd22478ecadfea7ea
Lauri Gustafsson <me@gustafla.space>
2019-11-09 14:45:27 +0200

    Add custom checkstyle rules and break them

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,29 @@ public class Main {
 
     public static void main(String[] args) {
 
+        for (int i=0; i<1; i++) {
+            for (int j=0; i<j; j++) {
+                System.out.println("virhe");
+            }
+        }
+
+        for (int i=0; i<1; i++) {
+        System.out.println("virhe");
+        }
+
+        if (true)
+        {
+            System.out.println("virhe");
+        }
+
+        if (true) {
+            if (true) {
+                if (false) {
+                    System.out.println("virhe");
+                }
+            }
+        }
+
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
