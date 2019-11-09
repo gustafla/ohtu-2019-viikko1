d6c64a3211b7a090fb14259e830a8c0bbad8f789
Lauri Gustafsson <me@gustafla.space>
2019-11-09 15:09:25 +0200

    Break a checkstyle rule

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,12 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
+        for (int i=0;i<1; i++) {
+            for (int j=0; j<1; j++) {
+                System.out.println("virhe");
+            }
+        }
+
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
